2c8d5161a67afeeb4ac93945212527e380af69ac
christalfepe <chrisperessutti@gmail.com>
2026-04-23 18:59:10 -0300

    Add de database

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.ma8fel3asds"
+    "revision": "0.g648b7ac418"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
